776f5e6c0944cacf2c39192b0f4b2ebf6f907954
Lucas Bianchezzi Oliveira <lucas.oliveira308@aluno.senai.br>
2023-12-12 16:49:29 -0300

    Alterado caminho da api para o da Azure

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -1,54 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
 /**
  * Módulo para trabalhar com apis. Disponibiliza as rotas da api bem como o serviço com a biblioteca axios
  */
 
-
-
 /**
  * Rota para o recurso Evento
  */
-export const eventsResource = '/Evento';
+export const eventsResource = "/Evento";
 /**
  * Rota para o recurso Listar Minhas Presenças
  */
-export const myEventsResource = '/PresencasEvento/ListarMinhas';
+export const myEventsResource = "/PresencasEvento/ListarMinhas";
 /**
  * Rota para o recurso Presenças Evento
  */
-export const presencesEventResource = '/PresencasEvento';
+export const presencesEventResource = "/PresencasEvento";
 /**
  * Rota para o recurso Presenças Evento
  */
-export const commentaryEventResource = '/ComentariosEvento';
+export const commentaryEventResource = "/ComentariosEvento";
 
 /**
  * Rota para o recurso Próximos Eventos
  */
-export const nextEventResource = '/Evento/ListarProximos';
+export const nextEventResource = "/Evento/ListarProximos";
 /**
  * Rota para o recurso Tipos de Eventos
  */
-export const eventsTypeResource = '/TiposEvento';
+export const eventsTypeResource = "/TiposEvento";
 /**
  * Rota para o recurso Instituição
  */
-export const institutionResource = '/Instituicao';
+export const institutionResource = "/Instituicao";
 /**
  * Rota para o recurso Login
  */
-export const loginResource = '/Login';
+export const loginResource = "/Login";
 
-const apiPort = '5000';
-const localApiUri = `http://localhost:${apiPort}/api`;
-const externallApiUri = ``;
+// const apiPort = '5000';
+// const localApiUri = `http://localhost:${apiPort}/api`;
+const externallApiUri = `https://eventlucas.azurewebsites.net/api`;
 // const externalApiUri = null;
 
 const api = axios.create({
-    baseURL: localApiUri
+  baseURL: externallApiUri,
 });
-
-
 
 export default api;
